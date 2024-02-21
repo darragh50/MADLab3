ec812558cc73b3db935e06eeeba39c473049de92
unknown <darraghrohan@gmail.com>
2024-02-21 11:20:47 +0000

    lab3

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,20 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { AComponent } from './a/a.component';
+import { BComponent } from './b/b.component';
+import { Action } from 'rxjs/internal/scheduler/Action';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, AComponent, BComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
+
+/*Data binding*/
 export class AppComponent {
-  title = 'my-first-app';
+  title:string = 'my-first-app';
+  name:string = 'Darragh';
+  age:number = 20;
 }
